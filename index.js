7bfafa93927a974b4ebf227739dd420e7c8f77ae
Henry Ripple <hripple46@gmail.com>
2023-05-07 23:16:10 -0500

    Happy Birthday git status

---
+++ index.js
@@ -0,0 +1,19 @@
+let cabin1 = document.querySelector(".img3");
+
+let cabin2 = document.querySelector(".img1");
+
+let cabin3 = document.querySelector(".img2");
+
+cabin1.addEventListener("click", function () {
+  window.location.href =
+    "https://www.airbnb.com/rooms/51633955?adults=2&category_tag=Tag%3A5348&children=0&enable_m3_private_room=true&infants=0&pets=1&check_in=2023-06-04&check_out=2023-06-09&federated_search_id=dd490c04-bd79-4479-accf-1c628c7e1edf&source_impression_id=p3_1683518925_YrzW%2FcTL39cBcB6a";
+});
+
+cabin2.addEventListener("click", function () {
+  window.location.href =
+    "https://www.airbnb.com/rooms/35241249?adults=2&category_tag=Tag%3A8186&enable_m3_private_room=true&pets=1&check_in=2023-09-08&federated_search_id=dd490c04-bd79-4479-accf-1c628c7e1edf&source_impression_id=p3_1683518130_NwUsQzGXY5pPqYZB&guests=1&check_out=2023-09-11";
+});
+cabin3.addEventListener("click", function () {
+  window.location.href =
+    "https://www.airbnb.com/rooms/782455484301769582?adults=2&category_tag=Tag%3A4104&enable_m3_private_room=true&pets=1&federated_search_id=dd490c04-bd79-4479-accf-1c628c7e1edf&source_impression_id=p3_1683518275_vEhJIlBVPwOoZP43&guests=1&check_in=2023-07-27&check_out=2023-07-31";
+});
